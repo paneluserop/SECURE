4d71e25cb4be1745634754de831f49a87b12804a
paneluserop <abhijeetpanda946@gmail.com>
2024-10-30 13:11:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const data = {
     "token": "" || process.env.token,
-    "mongo": ""|| process.env.mongo,
+    "mongo": "mongodb+srv://jeet:jeet2010@security.rg97z.mongodb.net/?retryWrites=true&w=majority&appName=Security"|| process.env.mongo,
  
 }
 console.log('got connected from database ..!!')
